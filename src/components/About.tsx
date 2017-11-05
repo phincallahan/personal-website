97cb44aeb0d6c8a8cc0459fe6dff9d9ebdea94f6
phincallahan <phincallahan14@gmail.com>
2017-11-05 16:25:20 +0100

    beautified about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,43 @@
 import * as React from 'react';
 
+const spotifyDiscoLink = "https://open.spotify.com/user/12141814312/playlist/38GqkAytTEqzeiycF2VlAM"
+
+const links = [
+    { 
+        name: "contact",
+        link: "mail"
+    },
+    {
+        name: "github",
+        link: "https://github.com/phincallahan"
+    },
+    {
+        name: "last.fm",
+        link: "https://www.last.fm/user/caphine"
+    },
+    {
+        name: "spotify",
+        link: "https://open.spotify.com/user/12141814312"
+    }
+];
+
 export const About = (props) => (
     <section className="about">
-        <h3>About</h3>
-        <p> phineas callahan is a: </p>
-        <ul>
-            <li> math major @ <a href="carleton.edu">carleton college</a></li>
-            <li> problem solver </li>
-            <li> music lover</li>
+        <p>
+            <b className="name"> Phineas Callahan </b> 
+            is a math major @ <a href="carleton.edu">Carleton College</a>. He
+            enjoys writing clean code, solving algorithmic problems, and jamming out
+            to <a href={spotifyDiscoLink}>disco</a>.
+        </p>
+
+        <ul className="links">
+            {
+                links.map(({name, link}) => (
+                    <li key={name}>
+                        <a href={link}>{name}</a>
+                    </li>
+                ))
+            }
         </ul>
     </section>
 )
