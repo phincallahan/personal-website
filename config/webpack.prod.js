37f83e3c5b8e4b7fa1bc1e5065e8c1ec907cbda5
phincallahan <phincallahan14@gmail.com>
2017-11-05 16:20:45 +0100

    fixed output path

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     output: {
         filename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, '../dist'),
         publicPath: '/'
     },
     resolve: {
